4f6ab1ffe0604104ba7e9747d2f3dd0c8f0103c5
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-08-22 15:54:33 +0530

    Update __init__.py

--- integrate/__init__.py
+++ integrate/__init__.py
@@ -1,13 +1,47 @@
 class ConnectToIntegrate:
+    def __init__(self):
+        # Dummy/session variables for placeholder purposes
+        self.uid = "dummy_uid"
+        self.actid = "dummy_actid"
+        self.api_session_key = "dummy_api_session_key"
+        self.ws_session_key = "dummy_ws_session_key"
+
     def login(self, *args, **kwargs):
-        pass
-    def set_session_keys(self, *args, **kwargs):
-        pass
+        # Set or refresh session keys here in real implementation
+        self.uid = "dummy_uid"
+        self.actid = "dummy_actid"
+        self.api_session_key = "dummy_api_session_key"
+        self.ws_session_key = "dummy_ws_session_key"
+
+    def set_session_keys(self, uid, actid, api_session_key, ws_session_key):
+        self.uid = uid
+        self.actid = actid
+        self.api_session_key = api_session_key
+        self.ws_session_key = ws_session_key
+
+    def get_session_keys(self):
+        # Return the four keys as expected by session_utils.py
+        return self.uid, self.actid, self.api_session_key, self.ws_session_key
 
 class IntegrateOrders:
     def __init__(self, conn):
         self.conn = conn
+
     def holdings(self):
         # Dummy data for testing
-        return {"data": [{"dp_qty": 10, "avg_buy_price": 100, "tradingsymbol": [{"exchange": "NSE", "tradingsymbol": "SBIN", "token": "123", "isin": "IN1234567890"}]}]}
-
+        return {
+            "data": [
+                {
+                    "dp_qty": 10,
+                    "avg_buy_price": 100,
+                    "tradingsymbol": [
+                        {
+                            "exchange": "NSE",
+                            "tradingsymbol": "SBIN",
+                            "token": "123",
+                            "isin": "IN1234567890"
+                        }
+                    ]
+                }
+            ]
+        }
