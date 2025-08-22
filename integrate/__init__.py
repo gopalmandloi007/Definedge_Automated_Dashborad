2555450c5cf7034dd6a94fcdc3dbabe00ae7d640
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-08-22 13:55:22 +0530

    Update __init__.py

--- integrate/__init__.py
+++ integrate/__init__.py
@@ -1,1 +1,13 @@
+class ConnectToIntegrate:
+    def login(self, *args, **kwargs):
+        pass
+    def set_session_keys(self, *args, **kwargs):
+        pass
+
+class IntegrateOrders:
+    def __init__(self, conn):
+        self.conn = conn
+    def holdings(self):
+        # Dummy data for testing
+        return {"data": [{"dp_qty": 10, "avg_buy_price": 100, "tradingsymbol": [{"exchange": "NSE", "tradingsymbol": "SBIN", "token": "123", "isin": "IN1234567890"}]}]}
 
